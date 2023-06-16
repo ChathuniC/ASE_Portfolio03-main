51e075e737207386003b24f8804f50559e66b2f1
ChathuniC <135264446+ChathuniC@users.noreply.github.com>
2023-06-16 13:30:24 +0530

    Add files via upload

---
+++ MVCAbominodo.java
@@ -0,0 +1,15 @@
+package MVC;
+
+import MVCController.ControllerAbominodo;
+
+/**
+ *
+ * @author Chathuni
+ */
+public class MVCAbominodo {
+    public static void main(String[] args){
+        ControllerAbominodo controller = new ControllerAbominodo();
+        controller.startApplication();
+    }
+    
+}
---
+++ User.java
@@ -0,0 +1,18 @@
+package strategypattern;
+
+/**
+ *
+ * @author Chathuni
+ */
+public class User {
+
+	public static void main (String[] args){
+		Context context = new Context();
+		context.execute();
+
+
+	}
+
+}
+
+    
